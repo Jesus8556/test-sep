f7ad1ec2468ccd7f003431eb5b2b496db8f98ef6
Jesus8556 <luis.gutierrez.m@tecsup.edu.pe>
2024-09-23 20:04:36 -0500

    test2

--- movie_analisis.py
+++ movie_analisis.py
@@ -4,14 +4,14 @@ import os
 
 app = Flask(__name__)
 
-# Inicializar la sesión de Spark
-spark = SparkSession.builder \
-    .appName("Movie Ratings Analysis") \
-    .getOrCreate()
-
 # Ruta para obtener todas las películas
 @app.route('/movies', methods=['GET'])
 def get_movies():
+    # Inicializar la sesión de Spark dentro de la función
+    spark = SparkSession.builder \
+        .appName("Movie Ratings Analysis") \
+        .getOrCreate()
+
     # Verificar si el archivo CSV ya ha sido procesado
     if not os.path.exists("movies_processed.json"):
         # Leer el archivo movies.csv
@@ -26,13 +26,11 @@ def get_movies():
     with open("movies_processed.json", "r") as f:
         movies_data = f.read()
 
+    # Cerrar la sesión de Spark después de procesar
+    spark.stop()
+
     # Devolver el JSON de películas
     return jsonify(eval(movies_data))
-
-# Cerrar la sesión de Spark al terminar la aplicación
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    spark.stop()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
